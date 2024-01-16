678c55a7715a20922d53beeedcfc35e344fb94e5
Orange <47792375+KChenDu@users.noreply.github.com>
2024-01-16 00:01:50 +0000

    fix setting error

--- frontend/main.py
+++ frontend/main.py
@@ -1,13 +1,12 @@
 import gradio as gr
 
 from gpt import predict
-from data.demo import EXAMPLES
 
 
 if __name__ == '__main__':
     gr.ChatInterface(predict,
                      chatbot=gr.Chatbot([(None, "Hi! How can I help you?")]),
-                     examples=EXAMPLES,
+                     examples=["Hello.", "Hello!", "How are you?"],
                      title='XOR Assistant',
                      description="Type your question and get response from the chatbot based on cross-lingual knowledge base!",
                      retry_btn=None,
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ## Initial tips for developers
-1. Do not push `data`, `images` and downloaded llm.
+1. Do not push `data`, `images` and LLM submodules.
 2. `test.ipynb` and `playground.py` have been added to `.gitignore`, one can use them for testing purposes.
 3. Place general utilities in `utils.py` and use them as much as possible.
 4. `main.py` is used for running all project services. Make sure your service works properly before commiting it.
